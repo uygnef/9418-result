77d87a11357e60ca91884e5f7ae91e6836a09c21
uygnef <97508691@qq.com>
2017-10-23 00:16:01 +1100

    test

---
+++ predict.py
@@ -0,0 +1,16 @@
+import numpy as np
+import random
+import sys
+
+file = open('predictions.txt')
+i = random.randint(1,100)
+output_file = sys.argv[1]
+output = open(output_file, 'w')
+for i in file.readlines():
+    result = i.strip().split(',')
+    if len(result) != 20:
+        result = result[:20]
+    result = str(np.argmax(result))+','
+    output.write(result)
+    
+
